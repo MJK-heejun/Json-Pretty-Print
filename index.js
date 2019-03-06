d6d9d07bafd751cc2307ed248731e8ff1a47a477
Kim <jakim@ea.com>
2019-03-06 15:56:09 -0800

    quick update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
 	$("#convertButton").click(function() {
 		var jsonStr = $("#input").val();
+		if($('#backslashTrim').is(':checked'))
+			jsonStr = jsonStr.replace(/\\/g, '');
+
+		if(jsonStr.length >= 2 && jsonStr.charAt(0) == '"' && jsonStr.charAt(jsonStr.lenght-1) == '"'){
+			jsonStr = jsonStr.substring(1, jsonStr.length - 1);
+		}
+		
 		try{
 			$("#output").text(JSON.stringify(JSON.parse(jsonStr),null,2));
 			$("#inputPage").hide();
